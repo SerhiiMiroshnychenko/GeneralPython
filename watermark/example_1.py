fe92422ff887c3fbe3ae63d0777667cf6960aa4a
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-17 13:28:30 +0200

    Watermark: example #1

---
+++ watermark/example_1.py
@@ -0,0 +1,20 @@
+# !pip install watermark
+
+from watermark import watermark
+print(watermark())
+
+"""
+Last updated: 2023-11-17T13:27:27.998947+02:00
+
+Python implementation: CPython
+Python version       : 3.11.4
+IPython version      : 8.17.2
+
+Compiler    : GCC 12.3.0
+OS          : Linux
+Release     : 6.2.0-36-generic
+Machine     : x86_64
+Processor   : x86_64
+CPU cores   : 8
+Architecture: 64bit
+"""
