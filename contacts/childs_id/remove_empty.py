73c2a737d2f83d58371719f8edec14c39a7cfc0b
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-12-09 21:03:12 +0200

    Update

---
+++ contacts/childs_id/remove_empty.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Шлях до файлу Excel
+input_file_path = "/home/smiroshnychenko/GeneralPython/contacts/childs_id/ChildsID.xlsx"
+
+# Зчитуємо дані з Excel
+df = pd.read_excel(input_file_path)
+
+# Видаляємо рядки, в яких значення стовпця 'child_ids/id' дорівнює False
+df = df[df['child_ids/id'] != False]
+
+# Записуємо змінений DataFrame назад у файл Excel
+output_file_path = "/home/smiroshnychenko/GeneralPython/contacts/childs_id/ChildsID-updated.xlsx"
+df.to_excel(output_file_path, index=False)
