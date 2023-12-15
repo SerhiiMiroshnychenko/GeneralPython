b9ee2671d8c04b1e0b4e0230420c97dcc5561c74
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-12-15 15:55:00 +0200

    Update

---
+++ data/bom/b1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Шляхи до файлів
+uniq_file_path = "/home/smiroshnychenko/Runex3/data/products/uniq.csv"
+bom_file_path = "/home/smiroshnychenko/Runex3/data/bom/product.template (3).csv"
+
+# Зчитуємо DataFrame з файлів
+uniq_df = pd.read_csv(uniq_file_path)
+bom_df = pd.read_csv(bom_file_path)
+
+# Обираємо рядки, де значення поля "default_code" співпадає з файлом uniq.csv
+result_df = bom_df[bom_df['default_code'].isin(uniq_df['default_code'])]
+
+# Записуємо результат у файл "result_bom.csv"
+result_df.to_csv("/home/smiroshnychenko/Runex3/data/bom/result_bom.csv", index=False)
+
+print("Results have been written to 'result_bom.csv'.")
