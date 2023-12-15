dcdc9d0e428a7c9c46f3fee23344a408f090cf08
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-12-15 15:56:01 +0200

    Update

---
+++ data/program/c8.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Зчитуємо дані з файлу sum1.csv
+sum1_df = pd.read_csv('/home/smiroshnychenko/Runex3/data/earlier/sum1.csv')
+
+# Зберігаємо дані у файл Excel
+sum1_df.to_excel('/home/smiroshnychenko/Runex3/data/earlier/sum1.xls', index=False, engine='openpyxl')
