626c6d4f7c33c49116af5aaace60285849ab1e68
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-12-15 09:42:45 +0200

    Update

---
+++ contacts/pricelists/remove_empty.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Шлях до CSV-файлу
+csv_path = '/home/smiroshnychenko/GeneralPython/contacts/pricelists/res.partner (24).csv'
+
+# Зчитуємо CSV-файл у DataFrame
+df = pd.read_csv(csv_path)
+
+# Рахуємо початкову кількість рядків
+initial_rows = df.shape[0]
+
+# Видаляємо рядки, де значення одного з полів дорівнює 0, False або відсутнє
+df = df[(df['id'] != 0) & (df['partner_product_pricelist/id'].notna()) & (df['partner_product_pricelist/id'] != False)]
+
+# Рахуємо кількість видалених рядків
+deleted_rows = initial_rows - df.shape[0]
+
+# Зберігаємо оновлений DataFrame у той же CSV-файл (заміна існуючого файлу)
+df.to_csv(csv_path, index=False)
+
+# Виводимо кількість видалених рядків
+print(f"Кількість видалених рядків: {deleted_rows}")
