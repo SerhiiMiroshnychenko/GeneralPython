12b25ff39f28a21218d0857d0c2bb6a393aacafe
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-12-18 09:27:57 +0200

    Update

---
+++ data/company/com3.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Шлях до файлу
+file_path = "/home/smiroshnychenko/Runex3/data/company/Contact (res.partner) (14).csv"
+
+# Зчитуємо DataFrame з файлу
+df = pd.read_csv(file_path)
+
+# Видаляємо рядки, де значення в колонці "is_company" не дорівнює "True"
+df = df[df["is_company"] == True]
+
+# Записуємо оновлений DataFrame назад у файл
+df.to_csv(file_path, index=False)
+
+print("Rows with 'is_company' not equal to 'True' have been removed.")
