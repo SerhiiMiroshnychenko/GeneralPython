625381b19fa08c50fa7a786295b0690e255f1fca
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-06 09:48:29 +0200

    Update

---
+++ data/pprogram/p1-1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Зчитування даних з файлів
+df_product_8 = pd.read_csv('/home/smiroshnychenko/Runex3/data/products/product.template-8.csv')
+df_product_16 = pd.read_csv('/home/smiroshnychenko/Runex3/data/products/Product(product.template)-16.csv')
+
+# Об'єднання даних за умовою, що значення в стовпці 'default_code' є однаковими
+df_merged = pd.merge(df_product_8, df_product_16, how='outer', on='default_code', suffixes=('-8', '-16'))
+
+# Вибірка потрібних полів для product_8-16.csv
+result_df = df_merged[["default_code", "name-8", "name-16", "id-8", "id-16"]]
+
+# Видалення дублікатів за унікальними значеннями поля 'default_code'
+result_df = result_df.drop_duplicates(subset='default_code')
+
+
+# Запис у файл product_8-16.csv
+result_df.to_csv('/home/smiroshnychenko/Runex3/data/products/product_8-16-1.csv', index=False)
