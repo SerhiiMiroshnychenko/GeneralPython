5627939cbdeceed4149d968222ea70270e950d2f
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-06 09:47:30 +0200

    Update

---
+++ data/duplicates/programs/d-name.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Зчитуємо дані з файлу Contact (res.partner)display_name.csv
+file_path = "/home/smiroshnychenko/Runex3/data/duplicates/data_files/Contact (res.partner)display_name.csv"
+df_contacts = pd.read_csv(file_path)
+
+# Знаходимо дублікати за полем "Display Name"
+duplicates_df = df_contacts[df_contacts.duplicated(subset=["Display Name"], keep=False)]
+
+# Створюємо новий DataFrame для dn_dupl.csv
+dn_dupl_df = pd.DataFrame(columns=["Display Name", "Number"])
+
+# Заповнюємо новий DataFrame
+for name, group_df in duplicates_df.groupby("Display Name"):
+    row_data = {"Display Name": name, "Number": len(group_df)}
+    dn_dupl_df = pd.concat([dn_dupl_df, pd.DataFrame([row_data])], ignore_index=True)
+
+# Записуємо результат у файл dn_dupl.csv
+dn_dupl_df.to_csv("/home/smiroshnychenko/Runex3/data/duplicates/data_files/dn_dupl.csv", index=False)
