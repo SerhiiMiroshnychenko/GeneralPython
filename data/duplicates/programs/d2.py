689b470b9e9711bbfd844077430e6d2c456a05c6
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-12 08:55:40 +0200

    Update

--- data/duplicates/programs/d2.py
+++ data/duplicates/programs/d2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Зчитуємо дані з файлу
-file_path = "/home/smiroshnychenko/Runex3/data/duplicates/data_files/Contact (res.partner) (9).csv"
+file_path = "Contact (res.partner) (9).csv"
 df = pd.read_csv(file_path)
 
 # Знаходимо дублікати за полем "name"
@@ -17,4 +17,4 @@ for name, group_df in duplicates_df.groupby("name"):
     duplicates_result_df = pd.concat([duplicates_result_df, pd.DataFrame([row_data])], ignore_index=True)
 
 # Записуємо результат у файл duplicates.csv
-duplicates_result_df.to_csv("/home/smiroshnychenko/Runex3/data/duplicates/data_files/duplicates2.csv", index=False)
+duplicates_result_df.to_csv("duplicates2.csv", index=False)
