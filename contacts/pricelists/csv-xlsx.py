75b40af2ccbc4cce9f5d067533aef0b277ab0d09
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-12 08:55:20 +0200

    Update

--- contacts/pricelists/csv-xlsx.py
+++ contacts/pricelists/csv-xlsx.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Шлях до файлу CSV
-csv_file_path = '/home/smiroshnychenko/GeneralPython/contacts/pricelists/Contact (res.partner) (15).csv'
+csv_file_path = 'Contact (res.partner) (15).csv'
 
 # Зчитуємо CSV у DataFrame
 df = pd.read_csv(csv_file_path)
 
 # Записуємо DataFrame у XLSX
-xlsx_file_path = '/home/smiroshnychenko/GeneralPython/contacts/pricelists/Contact(res.partner).xlsx'
+xlsx_file_path = 'Contact(res.partner).xlsx'
 df.to_excel(xlsx_file_path, index=False, engine='openpyxl')
 
 print(f"Файл {xlsx_file_path} успішно створено.")
