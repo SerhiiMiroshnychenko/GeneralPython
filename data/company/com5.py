b39027edf867828d16bc478bf2a47c50b225862e
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-12 08:55:01 +0200

    Update

--- data/company/com5.py
+++ data/company/com5.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Шляхи до файлів
-file_path1 = "/home/smiroshnychenko/Runex3/data/company/res.partner (12).csv"
-file_path2 = "/home/smiroshnychenko/Runex3/data/company/com_map.csv"
+file_path1 = "res.partner (12).csv"
+file_path2 = "com_map.csv"
 
 # Зчитуємо DataFrame з файлів
 df1 = pd.read_csv(file_path1)
@@ -15,6 +15,6 @@ merged_df = pd.merge(df1, df2, how='left', left_on='parent_id/id', right_on='old
 df1['parent_id/id'] = merged_df['new_id'].fillna(df1['parent_id/id'])
 
 # Записуємо результат у файл "res.partner (12)_updated.csv"
-df1.to_csv("/home/smiroshnychenko/Runex3/data/company/res.partner (12)_updated.csv", index=False)
+df1.to_csv("res.partner (12)_updated.csv", index=False)
 
 print("Updated file 'res.partner (12)_updated.csv' has been created.")
