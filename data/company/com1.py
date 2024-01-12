e342f6e59ac644b9ed7276c2ebceec8fc705666c
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-12 08:54:36 +0200

    Update

--- data/company/com1.py
+++ data/company/com1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Шлях до вхідного файлу
-input_file_path = "/home/smiroshnychenko/Runex3/data/company/res.partner (12).csv"
+input_file_path = "res.partner (12).csv"
 
 # Зчитуємо дані з файлу
 df = pd.read_csv(input_file_path)
@@ -13,7 +13,7 @@ new_df = df.copy()
 new_df["parent_id/id"] = new_df["parent_id/id"].astype(str).apply(lambda x: f"__import__.res_partner_legacy_{x.split('_')[-1]}")
 
 # Записуємо результат у файл "new_company.csv"
-output_file_path = "/home/smiroshnychenko/Runex3/data/company/new_company.csv"
+output_file_path = "new_company.csv"
 new_df.to_csv(output_file_path, index=False)
 
 print(f"File '{output_file_path}' has been created.")
