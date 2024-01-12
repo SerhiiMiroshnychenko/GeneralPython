ff70293a2dddfac3012dda5fe56297dfb5d8ee19
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-12 08:55:52 +0200

    Update

--- data/duplicates/programs/d2-xls.py
+++ data/duplicates/programs/d2-xls.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Зчитуємо дані з файлу sum1.csv
-sum1_df = pd.read_csv('/home/smiroshnychenko/Runex3/data/duplicates/data_files/duplicates2.csv')
+sum1_df = pd.read_csv('duplicates2.csv')
 
 # Зберігаємо дані у файл Excel
-sum1_df.to_excel('/home/smiroshnychenko/Runex3/data/duplicates/data_files/duplicates2.xls', index=False, engine='openpyxl')
+sum1_df.to_excel('duplicates2.xls', index=False, engine='openpyxl')
