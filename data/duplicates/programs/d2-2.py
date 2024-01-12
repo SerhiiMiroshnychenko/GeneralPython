1da062a24d70dccc443fffed3a1720f8e0a1b6fe
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-12 08:55:46 +0200

    Update

--- data/duplicates/programs/d2-2.py
+++ data/duplicates/programs/d2-2.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Зчитуємо дані з файлу duplicates.csv
-file_path = "/home/smiroshnychenko/Runex3/data/duplicates/data_files/duplicates2.csv"
+file_path = "duplicates2.csv"
 df_duplicates = pd.read_csv(file_path)
 
 # Відбираємо рядки, де значення полів "id 1" та "id 2" різні
 unique_duplicates_df = df_duplicates[df_duplicates['id 1'] != df_duplicates['id 2']]
 
 # Записуємо результат у файл uniq_dupl.csv
-unique_duplicates_df.to_csv("/home/smiroshnychenko/Runex3/data/duplicates/data_files/uniq_dupl2.csv", index=False)
+unique_duplicates_df.to_csv("uniq_dupl2.csv", index=False)
