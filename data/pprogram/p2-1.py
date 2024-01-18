bba272bda4faede0befe9bf1917cd88a14abc601
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-18 09:03:58 +0200

    Update

--- data/pprogram/p2-1.py
+++ data/pprogram/p2-1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Зчитуємо дані з файлу sum1.csv
-sum1_df = pd.read_csv('/home/smiroshnychenko/Runex3/data/products/product_8-16-1.csv')
+sum1_df = pd.read_csv('product_8-16-1.csv')
 
 # Зберігаємо дані у файл Excel
-sum1_df.to_excel('/home/smiroshnychenko/Runex3/data/products/product_8-16-1.xls', index=False, engine='openpyxl')
+sum1_df.to_excel('product_8-16-1.xls', index=False, engine='openpyxl')
