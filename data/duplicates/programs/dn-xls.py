2c69aaa1ccc07ddc6571065db0a884ad677bcc6f
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-18 09:02:01 +0200

    Update

--- data/duplicates/programs/dn-xls.py
+++ data/duplicates/programs/dn-xls.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Зчитуємо дані з файлу sum1.csv
-sum1_df = pd.read_csv('/home/smiroshnychenko/Runex3/data/duplicates/data_files/dn_dupl.csv')
+sum1_df = pd.read_csv('dn_dupl.csv')
 
 # Зберігаємо дані у файл Excel
-sum1_df.to_excel('/home/smiroshnychenko/Runex3/data/duplicates/data_files/dn_dupl.xls', index=False, engine='openpyxl')
+sum1_df.to_excel('dn_dupl.xls', index=False, engine='openpyxl')
