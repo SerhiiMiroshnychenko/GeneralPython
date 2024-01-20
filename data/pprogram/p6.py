a01e0651cf306414467d6363c88ffa82626a99ce
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-20 09:24:06 +0200

    Update

--- data/pprogram/p6.py
+++ data/pprogram/p6.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Зчитування даних з файлів
-df_full = pd.read_csv('/home/smiroshnychenko/Runex3/data/products/full.csv')
-df_uniq = pd.read_csv('/home/smiroshnychenko/Runex3/data/products/uniq.csv')
+df_full = pd.read_csv('full.csv')
+df_uniq = pd.read_csv('uniq.csv')
 
 # Відбір рядків, де значення поля 'default_code' збігається з uniq.csv
 df_extract = df_full[df_full['default_code'].isin(df_uniq['default_code'])]
 
 # Запис у файл extract.csv
-df_extract.to_csv('/home/smiroshnychenko/Runex3/data/products/extract.csv', index=False)
+df_extract.to_csv('extract.csv', index=False)
