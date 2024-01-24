7915a14be6af218425b17b22b5cb63d98e07796d
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-24 08:48:30 +0200

    Update

---
+++ data/duplicates/programs/ref-xls.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Зчитуємо дані з файлу sum1.csv
+sum1_df = pd.read_csv('res.partner (8).csv')
+
+# Зберігаємо дані у файл Excel
+sum1_df.to_excel('res.partner (8).xls', index=False, engine='openpyxl')
