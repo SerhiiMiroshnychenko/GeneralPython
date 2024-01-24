3c832abe159cfc2de496a2f28f714f7646639d9c
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-24 08:48:36 +0200

    Update

--- contacts/childs_id/remove_empty.py
+++ contacts/childs_id/remove_empty.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Шлях до файлу Excel
-input_file_path = "/home/smiroshnychenko/GeneralPython/contacts/childs_id/ChildsID-2.xlsx"
+input_file_path = "ChildsID-2.xlsx"
 
 # Зчитуємо дані з Excel
 df = pd.read_excel(input_file_path)
@@ -10,5 +10,5 @@ df = pd.read_excel(input_file_path)
 df = df[df['child_ids/id'] != False]
 
 # Записуємо змінений DataFrame назад у файл Excel
-output_file_path = "/home/smiroshnychenko/GeneralPython/contacts/childs_id/ChildsID-2-updated.xlsx"
+output_file_path = "ChildsID-2-updated.xlsx"
 df.to_excel(output_file_path, index=False)
