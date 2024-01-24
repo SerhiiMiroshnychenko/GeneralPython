e44bc3747fb96566a8f1cfa66ee042612c44b3e7
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-24 08:48:06 +0200

    Update

--- contacts/companies/pproperty_account_mapping.py
+++ contacts/companies/pproperty_account_mapping.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Шлях до файлу Excel
-input_file_path = "/home/smiroshnychenko/GeneralPython/contacts/companies/COMPANIES-5.xlsx"
+input_file_path = "COMPANIES-5.xlsx"
 
 # Зчитуємо дані з Excel
 df = pd.read_excel(input_file_path)
@@ -13,5 +13,5 @@ df['property_account_payable/id'] = 'l10n_se.1_a2440'
 df['property_account_receivable/id'] = 'l10n_se.1_a1510'
 
 # Записуємо змінений DataFrame назад у файл Excel
-output_file_path = "/home/smiroshnychenko/GeneralPython/contacts/companies/COMPANIES-6.xlsx"
+output_file_path = "COMPANIES-6.xlsx"
 df.to_excel(output_file_path, index=False)
