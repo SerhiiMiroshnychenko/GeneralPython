a47f2d2057af43b54405ed8476177c15b6e4c621
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-01-26 08:44:28 +0200

    Update

---
+++ data/program/с6.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import re
+
+# Зчитування файлу res.partner.csv
+file_path = "Contact (res.partner) (7).csv"
+df = pd.read_csv(file_path)
+
+
+# Функція для видобуття перших п'яти цифр з поля "id"
+def extract_id_number(row):
+    match = re.search(r'\d{5}|\d{4}|\d{3}', str(row['id']))
+    return int(match.group()) if match else None
+
+
+# Додавання нового стовпця "id_number"
+df['id_number'] = df.apply(extract_id_number, axis=1)
+
+# Вибірка потрібних полів для imp.csv
+selected_columns = ['id_number',
+                    "id", "name", "property_product_pricelist/id", "property_supplier_payment_term_id/id",
+                    "property_payment_term_id/id"
+                    ]
+df_imp = df[selected_columns]
+
+# Запис у файл imp.csv
+df_imp.to_csv('imp3.csv', index=False)
