dde8c0dcb435f3d38e4a90bd057c85562f763a19
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-27 08:43:45 +0200

    Update

---
+++ data/Scripts/set_lot_number.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import re
+
+# Завантажуємо дані з Excel файлу
+file_path = '/home/smiroshnychenko/Coffee-Project/FTR/Quants (stock.quant) (33).xlsx'
+df = pd.read_excel(file_path)
+
+
+# Функція для виділення даних в квадратних дужках
+def extract_data(text):
+    match = re.search(r'\[(.*?)\]', text)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+# Застосовуємо функцію до стовбця 'Product' і створюємо новий стовбець 'Lot/Serial Number'
+df['Lot/Serial Number'] = df['Product'].apply(lambda x: extract_data(x))
+
+# Зберігаємо зміни у файлі
+df.to_excel(file_path, index=False)
