2d5b1b68a0fff16a7ed25abd7cce32f9ae9edb9b
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-08-26 10:10:12 +0300

    Turtle

--- match_case/match_case_14.py
+++ match_case/match_case_14.py
@@ -1,1 +1,20 @@
-# https://www.geeksforgeeks.org/python-match-case-statement/
+# match case with python dictionary
+def run_match(dictionary):
+    # match case
+    match dictionary:
+        # pattern 1
+        case {"name": n, "age": a}:
+            print(f"Name:{n}, Age:{a}")
+        # pattern 2
+        case {"name": n, "salary": s}:
+            print(f"Name:{n}, Salary:{s}")
+        # default pattern
+        case _:
+            print("Data does not exist")
+
+
+if __name__ == "__main__":
+    run_match({"name": "Jay", "age": 24})
+    run_match({"name": "Ed", "salary": 25000})
+    run_match({"name": "Al", "age": 27})
+    run_match({})
